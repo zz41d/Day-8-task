4dcfa8ee1f5d23efab521cc53b99d0546959ddc6
zz41d <zaidali9347@gmail.com>
2024-02-19 18:34:58 +0530

    first commit

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h1>Welcome to My Nuxt.js App</h1>
+      <h2>{{ heading }}</h2>
+      <button @click="incrementCounter">Increment</button>
+      <p>Counter: {{ counter }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+
+  
+  const counter = ref(0);
+  const heading = 'Counter App';
+  
+  const incrementCounter = () => {
+    counter.value++;
+  };
+  </script>
+  
+  <style>
+
+  </style>
+  
